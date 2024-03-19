50a278b7edab287fcb12ba47beb388c81dfe67b6
unknown <aibhisarkar123@gmail.com>
2024-03-19 14:53:33 +0530

    First Commit

--- App.tsx
+++ App.tsx
@@ -77,7 +77,7 @@ function App(): React.JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            Edit aibhi <Text style={styles.highlight}>App.tsx</Text> git addto change this
             screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
@@ -87,7 +87,7 @@ function App(): React.JSX.Element {
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
-            Read the docs to discover what to do next:
+            Aibhi
           </Section>
           
         </View>
